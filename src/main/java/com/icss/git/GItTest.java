5d6d08dc550facdc263cbd81f29ad6934bc56053
hasee <hasee@163.com>
2022-03-23 17:09:51 +0800

    second commit

--- src/main/java/com/icss/git/GItTest.java
+++ src/main/java/com/icss/git/GItTest.java
@@ -3,5 +3,6 @@ package com.icss.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("helllo git!");
+        System.out.println("helllo git!");
     }
 }
