45caa7402fb872c2881955aa31400d4bb8010962
hasee <hasee@163.com>
2022-03-23 17:21:23 +0800

    hot-fix commit1

--- src/main/java/com/icss/git/GItTest.java
+++ src/main/java/com/icss/git/GItTest.java
@@ -3,5 +3,8 @@ package com.icss.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("helllo git!");
+        System.out.println("helllo git!");
+        System.out.println("helllo git!");
+        System.out.println("helllo git4!");
     }
 }
