d45d4564f123ba8381fcf514a1e280b71ab59b4d
hasee <hasee@163.com>
2022-03-23 17:10:40 +0800

    third commit

--- src/main/java/com/icss/git/GItTest.java
+++ src/main/java/com/icss/git/GItTest.java
@@ -4,5 +4,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("helllo git!");
         System.out.println("helllo git!");
+        System.out.println("helllo git3!");
     }
 }
